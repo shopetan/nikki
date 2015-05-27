ad8734beef14934b30d3d1ef767efa858c1156ff
shopetan <shoppe_shopetan@yahoo.co.jp>
2015-05-27 10:09:56 +0900

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+# coding: euc-jp
+class HomeController < ApplicationController
+  def index
+    # 戻り値が true ならサインイン済み。
+    user_signed_in?
+    # サインインしている全ユーザー取得
+    current_user
+    # ユーザーのセッション情報
+    user_session
+  end
+end
